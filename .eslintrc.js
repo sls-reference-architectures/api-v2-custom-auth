6a4231c7079a1f46c29d48f664e91f76a5186b88
Seth Orell <seth.orell@gmail.com>
2022-09-17 12:49:10 -0500

    npm init and basic libs

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    jest: true,
+    node: true,
+  },
+  extends: ['airbnb-base', 'airbnb-typescript/base', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: ['no-only-tests'],
+  root: true,
+  rules: {
+    'import/extensions': 0,
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['off'],
+    'no-only-tests/no-only-tests': 'error',
+    'no-console': 2,
+  },
+  settings: {
+    'import/resolver': 'node',
+  },
+};
